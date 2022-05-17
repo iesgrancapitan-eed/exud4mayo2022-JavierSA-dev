6adaf52c4cafd30e16ab4c5ce13041f2a9e5ab56
javierSA <javilondo56@gmail.com>
2022-05-17 09:22:18 +0200

    refactor: GUAUUUUUUU

--- exud4mayo2022-JavierSA-dev-main/src/examenOptimizacionYDocumentacion/Perro.java
+++ exud4mayo2022-JavierSA-dev-main/src/examenOptimizacionYDocumentacion/Perro.java
@@ -1,30 +1,33 @@
 package com.jsl.examen.OptimizacionYDocumentacion;
 
+import examenOptimizacionYDocumentacion.EdadErroneaException;
+
 public class Perro {
-	private int age;
-	private String name;
+    private int age;
+    private String name;
+    private final String LADRAR = "GUAUUUUUUUUUU";
 
-	public Perro(String name, int age) throws EdadErroneaException {
-		setAge(age);
-		this.name = name;
-	}
+    public Perro(String name, int age) throws EdadErroneaException {
+        setAge(age);
+        this.name = name;
+    }
 
-	public void setAge(int age) throws EdadErroneaException {
-		if (age < 0)
-			throw new EdadErroneaException("La edad no puede ser negativa");
-		this.age = age;
-	}
+    public void setAge(int age) throws EdadErroneaException {
+        if (age < 0)
+            throw new EdadErroneaException("La edad no puede ser negativa");
+        this.age = age;
+    }
 
-	public int getAge() {
-		return this.age;
-	}
+    public int getAge() {
+        return this.age;
+    }
 
-	public String ladrar() {
-		return "GUAUUUUUUUUUU";
-	}
+    public String ladrar() {
+        return LADRAR;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
 }
