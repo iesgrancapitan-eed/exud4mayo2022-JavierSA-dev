6e26b85a3658106db368ae7737bccc47c3dab4e8
javierSA <javilondo56@gmail.com>
2022-05-17 09:31:15 +0200

    docs: comentarios javadoc

--- exud4mayo2022-JavierSA-dev-main/src/examenOptimizacionYDocumentacion/Perro.java
+++ exud4mayo2022-JavierSA-dev-main/src/examenOptimizacionYDocumentacion/Perro.java
@@ -1,27 +1,51 @@
 package com.jsl.examen.OptimizacionYDocumentacion;
 
-import examenOptimizacionYDocumentacion.EdadErroneaException;
 
+/**
+ * Perro con edad y nombre heredado de la super clase
+ */
 public class Perro extends Animal {
-    private int age;
+  
+    /**
+     * Edad del perro
+     */
+    private int age; 
+    /**
+     * Ladrido del perro
+     */
     private final String LADRAR = "GUAUUUUUUUUUU";
-
+    /**
+     * Crea un perro con nombre y edad
+     * @param name
+     * @param age
+     * @throws EdadErroneaException
+     */
     public Perro(String name, int age) throws EdadErroneaException {
         super();
         setAge(age);
         this.name = name;
     }
-
+    /**
+     * Modifica la edad
+     * @param age
+     * @throws EdadErroneaException
+     */
     public void setAge(int age) throws EdadErroneaException {
         if (age < 0)
             throw new EdadErroneaException("La edad no puede ser negativa");
         this.age = age;
     }
-
+   /**
+    * Devuelve la edad
+    * @return
+    */
     public int getAge() {
         return this.age;
     }
-
+    /**
+     * Devuelve el ladrido
+     * @return
+     */
     public String ladrar() {
         return LADRAR;
     }
